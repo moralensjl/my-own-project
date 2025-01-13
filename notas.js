6cf9656e6ce992e7306cae605cbe5f6ac77df9c6
Moralens Jean-Louis <woodleyj49@gmail.com>
2025-01-13 00:24:08 +0000

    Experimentando con if y else en js

---
+++ notas.js
@@ -0,0 +1,23 @@
+let nota = 4
+
+if (nota >= 90 && nota <= 100) {
+    console.log('Exelente (A)')
+
+} else if (nota >= 80 && nota <= 89) {
+    console.log('Muy bueno (B)')
+
+} else if (nota >= 70 && nota <= 79) {
+    console.log('Bueno (C)')
+
+} else if (nota >= 60 && nota <= 69) {
+    console.log('Deficiente (D)')
+
+} else if (nota >= 50 && nota <= 59) {
+    console.log('No califica (E)')
+
+} else if (nota >= 40 && nota <= 49){
+    console.log('Fracasado (F)')
+
+} else {
+    console.log('Dicho numero no esta en el rango de nota')
+}
